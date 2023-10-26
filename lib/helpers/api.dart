f96377b65b372172b1be0ea7d44cc6c0a2eb913d
aufasyaihan <aufa.azzahidi@mhs.unsoed.com>
2023-10-26 23:04:32 +0700

    hapusin comment

--- lib/helpers/api.dart
+++ lib/helpers/api.dart
@@ -32,54 +32,6 @@ class Api {
     return responseJson;
   }
 
-  // Future<dynamic> put(dynamic url, dynamic data) async {
-  //   var token = await UserInfo().getToken();
-  //   var responseJson;
-  //   try {
-  //     final response = await http.put(
-  //       Uri.parse(url),
-  //       body: data,
-  //       headers: {HttpHeaders.authorizationHeader: "Bearer $token"},
-  //     );
-  //     responseJson = _returnResponse(response);
-  //   } on SocketException {
-  //     throw FetchDataException('No Internet connection');
-  //   }
-  //   return responseJson;
-  // }
-
-  // Future<dynamic> put(dynamic url, dynamic data) async {
-  //   var token = await UserInfo().getToken();
-  //   var responseJson;
-  //   print("datanya adalah : \n $data");
-  //   try {
-  //     final response = await http.put(Uri.parse(url),
-  //         headers: {"Content-Type": "application/json"}, body: data);
-  //     responseJson = _returnResponse(response);
-  //     print("HTTP Status Code: ${response.statusCode}");
-  //     print("Response Body: ${response.body}");
-  //   } on SocketException {
-  //     throw FetchDataException('No Internet connection');
-  //   }
-  //   return responseJson;
-  // }
-
-  // Future<dynamic> put(dynamic url, dynamic data) async {
-  //   var token = await UserInfo().getToken();
-  //   var responseJson;
-  //   try {
-  //     final response = await http.put(
-  //       Uri.parse(url),
-  //       headers: {'Content-Type': 'application/json'},
-  //       body: json.encode(data),
-  //     );
-  //     responseJson = _returnResponse(response);
-  //   } on SocketException {
-  //     throw FetchDataException('No Internet connection');
-  //   }
-  //   return responseJson;
-  // }
-
   Future<dynamic> put(dynamic url, dynamic data) async {
     var token = await UserInfo().getToken();
     var responseJson;
